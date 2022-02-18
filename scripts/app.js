ad757cede64e5cdfcecb91db1aa73e5bfba0ddb5
Edward Foulds <ed2494@googlemail.com>
2022-02-18 16:17:56 +0000

    WHile loop to check for possible exits added

--- scripts/app.js
+++ scripts/app.js
@@ -24,12 +24,18 @@ createGrid();
 
 
 
+// 2) Create a start and finish square
+gridArray[0].classList.add('starting-square', 'successful-path');
+gridArray[gridArray.length-1].classList.add('finishing-square');
 
-// 2) Create successful path
 
-// Set variables
-let currentIndex = 0;
-let randomNumber = null;
+
+
+
+
+
+
+// 3) Create successful path
 
 // Generates a random number for new direction
 function generateRandomNumber() {
@@ -58,33 +64,42 @@ function checkIfWallPresent(direction, currentIndex) {
   const yPosition = Math.floor(currentIndex / width);
   
   // Considers direction and current index x/y positions to see if it can move.
-  // If false is returned, then cannot move in that direction
+  // Returns false if no wall in that direction
   switch (direction) {
     case 'right':
-      return (xPosition < (width - 1))
+      return (xPosition >= (width - 1))
       break;
     case 'left':
-      return (xPosition > 0);
+      return (xPosition <= 0);
       break;
     case 'up':
-      return (yPosition > 0);
+      return (yPosition <= 0);
       break;
     case 'down':
-      return (yPosition < (width - 1));
+      return (yPosition >= (width - 1));
       break;
   }
 }
 
-// Function to check if index has successful path class
+// Function to check if index has successful path class. Returns false if class not present
 function checkIfSuccessfulPathClass(index) {
-  console.log(gridArray[index]);
-  return gridArray[index].classList.contains('successfulPath');
+  return gridArray[index].classList.contains('successful-path');
 }
 
-// Function to check if index has fake path class
+// Function to check if index has fake path class. Returns false if class not present
 function checkIfFakePathClass(index) {
-  console.log(gridArray[index]);
-  return gridArray[index].classList.contains('fakePath');
+  return gridArray[index].classList.contains('fake-path');
+}
+
+// Checks if index has any possible exits that do not contain a successfulPath or fakePath class or if its a wall
+function checkPossibleExit(index) {
+  return ((checkIfWallPresent('up', index) === false && checkIfFakePathClass(moveUp(index)) === false && checkIfSuccessfulPathClass(moveUp(index)) === false) || 
+  
+    (checkIfWallPresent('right', index) === false && checkIfFakePathClass(moveRight(index)) === false && checkIfSuccessfulPathClass(moveRight(index)) === false) ||
+  
+    (checkIfWallPresent('down', index) === false && checkIfFakePathClass(moveDown(index)) === false && checkIfSuccessfulPathClass(moveDown(index)) === false) ||
+  
+    (checkIfWallPresent('left', index) === false && checkIfFakePathClass(moveLeft(index)) === false && checkIfSuccessfulPathClass(moveLeft(index)) === false) )
 }
 
 
@@ -92,27 +107,75 @@ function checkIfFakePathClass(index) {
 
 
 
-console.log(gridArray);
-gridArray[2].classList.add('successfulPath');
-console.log(checkIfSuccessfulPathClass(2));
-gridArray[2].classList.add('fakePath');
-console.log(checkIfFakePathClass(2));
 
-// console.log(checkIfWallPresent('down',7));
 
-// let number = 0;
-// const xPosition = number % width;
-// const yPosition = Math.floor(number / width);
-// console.log('Can move left: ', xPosition > 0);
-// console.log('Can move right: ', xPosition < (width - 1));
-// console.log('Can move up: ', yPosition > 0);
-// console.log('Can move down: ', yPosition < (width - 1));
-// console.log()
+let successfulPathArray = [0, 1, 2, 12, 22, 23, 24, 34];
+let fakePathArray =[];
+let currentIndex = 34;
+let randomNumber = null;
+let newIndex = null;
 
-// console.log(moveRight(currentIndex));
-// console.log(moveLeft(currentIndex));
-// console.log(moveUp(currentIndex));
-// console.log(moveDown(currentIndex));
+gridArray[1].classList.add('successful-path');
+gridArray[2].classList.add('successful-path');
+gridArray[12].classList.add('successful-path');
+gridArray[22].classList.add('successful-path');
+gridArray[23].classList.add('successful-path');
+gridArray[24].classList.add('successful-path');
+gridArray[34].classList.add('successful-path');
 
-// randomNumber = generateRandomNumber();
-// console.log(randomNumber);
+gridArray[33].classList.add('successful-path');
+gridArray[44].classList.add('successful-path');
+gridArray[35].classList.add('successful-path');
+gridArray[25].classList.add('successful-path');
+gridArray[14].classList.add('successful-path');
+
+
+
+
+
+
+
+
+// Checking if there is a possible exit
+while (checkPossibleExit(currentIndex) === false) {
+  gridArray[currentIndex].classList.remove('successful-path')
+  gridArray[currentIndex].classList.add('fake-path');
+  fakePathArray.push(currentIndex);
+  successfulPathArray.pop();
+  currentIndex = successfulPathArray[successfulPathArray.length - 1];
+}
+
+console.log("Successful Path Array: ", successfulPathArray);
+console.log(successfulPathArray[successfulPathArray.length - 1])
+console.log(currentIndex);
+console.log(fakePathArray);
+
+
+
+// // console.log(gridArray);
+
+
+
+
+
+
+// // console.log(checkIfWallPresent('up',99));
+
+// console.log("Possilbe Exit: ", checkPossibleExit(12));
+
+// // let number = 0;
+// // const xPosition = number % width;
+// // const yPosition = Math.floor(number / width);
+// // console.log('Can move left: ', xPosition > 0);
+// // console.log('Can move right: ', xPosition < (width - 1));
+// // console.log('Can move up: ', yPosition > 0);
+// // console.log('Can move down: ', yPosition < (width - 1));
+// // console.log()
+
+// // console.log(moveRight(currentIndex));
+// // console.log(moveLeft(currentIndex));
+// // console.log(moveUp(currentIndex));
+// // console.log(moveDown(currentIndex));
+
+// // randomNumber = generateRandomNumber();
+// // console.log(randomNumber);
